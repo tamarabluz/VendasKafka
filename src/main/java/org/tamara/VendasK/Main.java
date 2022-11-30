0ba094aaf169f20649050b2e259d1f3bc6599465
tamara luz <tamarabluz@gmail.com>
2022-11-30 11:29:09 -0300

    Classe Vendas

---
+++ src/main/java/org/tamara/VendasK/Main.java
@@ -0,0 +1,7 @@
+package org.tamara.VendasK;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/tamara/VendasK/Model/Vendas.java
@@ -0,0 +1,70 @@
+package org.tamara.VendasK.Model;
+
+import java.math.BigDecimal;
+
+public class Vendas {
+    public Vendas(Long operacao, Long cliente, Integer quantidadeIngressos, BigDecimal valorTolal, String status) {
+        this.operacao = operacao;
+        this.cliente = cliente;
+        this.quantidadeIngressos = quantidadeIngressos;
+        this.valorTolal = valorTolal;
+        this.status = status;
+    }
+
+    private Long operacao;
+    private Long cliente;
+    private Integer quantidadeIngressos;
+    private BigDecimal valorTolal;
+    private String status;
+
+    public Long getOperacao() {
+        return operacao;
+    }
+
+    public void setOperacao(Long operacao) {
+        this.operacao = operacao;
+    }
+
+    public Long getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Long cliente) {
+        this.cliente = cliente;
+    }
+
+    public Integer getQuantidadeIngressos() {
+        return quantidadeIngressos;
+    }
+
+    public void setQuantidadeIngressos(Integer quantidadeIngressos) {
+        this.quantidadeIngressos = quantidadeIngressos;
+    }
+
+    public BigDecimal getValorTolal() {
+        return valorTolal;
+    }
+
+    public void setValorTolal(BigDecimal valorTolal) {
+        this.valorTolal = valorTolal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Vendas{" +
+                "operacao=" + operacao +
+                ", cliente=" + cliente +
+                ", quantidadeIngressos=" + quantidadeIngressos +
+                ", valorTolal=" + valorTolal +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
