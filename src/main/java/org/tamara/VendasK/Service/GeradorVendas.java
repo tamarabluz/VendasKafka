cfc9c07c02db2260e6f26e5c860a97e9ff6dddba
tamara luz <tamarabluz@gmail.com>
2022-11-30 13:14:39 -0300

    Classe Gerador Vendas com Kakfa

--- src/main/java/org/tamara/VendasK/Service/GeradorVendas.java
+++ src/main/java/org/tamara/VendasK/Service/GeradorVendas.java
@@ -2,9 +2,14 @@ package org.tamara.VendasK.Service;
 
 
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.tamara.VendasK.Model.Vendas;
 
 import java.math.BigDecimal;
+import java.util.Properties;
 import java.util.Random;
 
 public class GeradorVendas {
@@ -13,9 +18,21 @@ public class GeradorVendas {
     private static long operaaco = 0;
     private static BigDecimal valorIngresso = BigDecimal.valueOf(500);
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        Vendas vendas = geraVendas();
+        try(KafkaProducer<String, Vendas> producer = new KafkaProducer<String, Vendas>(properties)) {
+
+            while (true) {
+                Vendas vendas = geraVendas();
+                ProducerRecord<String, Vendas> record = new ProducerRecord<String, Vendas>("vendas-ingressos", vendas );
+                producer.send(record);
+                Thread.sleep(200);
+            }
+        }
     }
     private static Vendas geraVendas(){
         long cliente = rand.nextLong();
